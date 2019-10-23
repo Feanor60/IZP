6f90d2a30fe072884bea575ef6f2eab3c67d28bb
Bubela Vojtech <xbubel08@merlin.fit.vutbr.cz>
2019-10-23 12:56:40 +0200

    Add project1

---
+++ proj1/proj1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char sez_input[100];
+    printf("Hello world!\n");
+    
+    fgets(sez_input, 100, stdin);
+
+    printf("%s", sez_input);
+    printf("%s", argv[1]);
+    return 0;
+}
