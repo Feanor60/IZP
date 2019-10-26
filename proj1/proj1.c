ecb9c9dc72f9d04f24127f3b8ad49b9b986b0d20
Bubela Vojtech <xbubel08@merlin.fit.vutbr.cz>
2019-10-26 20:24:40 +0200

    proj1 beginnings

--- proj1/proj1.c
+++ proj1/proj1.c
@@ -3,17 +3,40 @@
 
 int main(int argc, char *argv[])
 {
-    char sez_input[100];
-    
+    char seznam_input[100];
+    //string to store stdin
+    char compare_char_stdin;
+    //char to compare parameters with
+    char compare_char_param;
+    //char to compare stdin with
+    char search_parameters[100];
+    search_parameters = argv[1];
+
     if(argc==2)
+    //checking for appropriate amount of parameters
     {
-        fgets(sez_input, 100, stdin)
-        
-        
+        while(fgets(seznam_input,100,stdin)!=NULL)
+        //running as long as fgets reads from seznam.txt
+       {
+            unsigned int pos_char = 0;
+            //position of char comparing
+            compare_char_stdin = seznam_input[pos_char];
+            compare_char_param = search_parameters[pos_char];
+
+            if(strstr(compare_char_stdin,compare_char_param))
+            {
+                if(strstr())
+            }
+       }
     }
-    printf("uh oh: %s\n", sez_input);
-    printf("uh ohh: %s\n", argv[1]);
-    printf("uh ohhh: %d\n",argc);
+
+    fgets(seznam_input,100,stdin);
+    compare_char = seznam_input[0];
+    
+    printf("prvni znak seznam.txt: %c\n", compare_char);
+    printf("output seznam.txt: %s\n", seznam_input);
+    printf("output argumentu pro hledani: %s\n", argv[1]);
+    printf("pocet argumentu: %d\n",argc);
 
     return 0;
 }
