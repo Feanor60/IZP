784713f36ac47a1b3d4ace9e7ac7c066a2795ddd
Bubela Vojtech <xbubel08@merlin.fit.vutbr.cz>
2019-10-30 21:02:08 +0100

    if argc<2 covered

--- proj1/proj1.c
+++ proj1/proj1.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
 #include <string.h>
 
+
+void vypis_kontakty()
+{
+    char from_seznam[101];
+    int radky = 1; //pocitadlo na radky
+
+    while(fgets(from_seznam, 101, stdin) != 0)
+    {
+        from_seznam[strlen(from_seznam) - 1] = '\0';
+        
+        if(radky % 2 == 1) //pokud je zbytek po deleni dvema 1 tak se jedna o lichy radek
+        {
+            printf("%s, ", from_seznam);
+        }else{
+            printf("%s\n", from_seznam);
+        }
+    radky++; //posunu se na dalsi radek
+    }       
+}
+
 int main(int argc, char *argv[])
 {
-    //nacteni a kontrola vyhledavacich parametru
     char *vyhledavaci_parametr = argv[1];
 
+    //nacteni a kontrola vyhledavacich parametru
     if(vyhledavaci_parametr==NULL)
     {
-        //vypsat vsechny kontakty
+       vypis_kontakty();
     }
 
-    printf("vyhledavaci parametr je: %s\n", vyhledavaci_parametr);
+    
+    //cyklus pro hledani v seznam.txt
+    
+    //vypis nalezenych kontaktu
 
-    //cyklus pro hledani v seznam.txt, vypis nalezenych kontaktu
+    //nic nenalezeno
 
     
 
-   printf("%d\n", argc);
+    printf("pocet argumentu v argc: %d\n", argc);
 
     return 0;
 }
