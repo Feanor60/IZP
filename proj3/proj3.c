3693f21e3b89fe32835705f1d0a4d1088fbcdda1
Feanor69 <vojta@bubela.cz>
2019-12-10 19:37:07 +0100

    --help and loading data to structure done

--- proj3/proj3.c
+++ proj3/proj3.c
@@ -8,17 +8,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-//#TODO --help
-void help();
-
-//#TODO --test
+#define test_fce printf("test\n");
+#define initial_value 0
 
 typedef struct {
   int rows;
   int cols;
   unsigned char *cells;
 } Map;
+
+void delete(char*c);//delete later
+
+void help();
+
+Map Map_ctor(int rows, int cols);
+
+void Map_init(Map *m, char*file);
+
+void Map_dtor(Map *m);
+
+bool Test_map(Map *m, int r, int c);
+
+bool is_border(Map *map, int r, int c, int border);
 
 int main(int argc, char* argv[])
 {
@@ -36,11 +49,15 @@ int main(int argc, char* argv[])
 
     FILE *fp;
     char c;
-    fprintf(stdout,"argv[1]: %s\n", argv[argc-1]);
-    // char *temp = argv[argc - 1];
+    char *file = "bludiste.txt";
 
-    if(strcmp(argv[argc-1],"bludiste.txt"))
-        fp = fopen("bludiste.txt","r");
+    if(strcmp(argv[argc-1],file)==0)
+    {
+        fp = fopen(file,"r");
+    }else{
+        fprintf(stderr,"failed to open file\n");
+        return 1;
+    }
 
     int rows;
     int cols;
@@ -52,18 +69,112 @@ int main(int argc, char* argv[])
     }else{
         fscanf(fp,"%d %d", &rows, &cols);
         c = fgetc(fp);
-        while(c != EOF)
-        {
-            printf("%c", c);
-            c = fgetc(fp);
-        }
-        printf("\n");
     }
 
+    Map m = Map_ctor(rows,cols);
+
+    Map_init(&m, file);
+
+    printf("number of rows: %d\nnumber of cols: %d\n", m.rows, m.cols);
+
+    // if(!Test_map(&m))
+    // {
+    //     fprintf(stderr,"Invalid map\n");
+    //     return 1;
+    // }    
+
+    delete(&c);
+    fclose(fp);
+    Map_dtor(&m);
     return 0;
 }
 
-void Map_init();
+void delete(char *c)
+{
+    *c = 0;
+}
+
+Map Map_ctor(int rows, int cols)
+{
+    Map m;
+    m.rows = rows;
+    m.cols = cols;
+
+    m.cells = malloc(rows * cols * sizeof(unsigned char));
+    if(m.cells == NULL)
+        fprintf(stderr,"failed to allocate memory\n");
+
+    return m;
+}
+
+void Map_init(Map *m, char*file)
+{
+    test_fce
+    FILE *fpt;
+    fpt = fopen(file,"r");
+
+    char c;
+    while(c != '\n')
+    {
+        c = fgetc(fpt);
+    }
+
+    for(int i = 0; c != EOF; i++)
+    {
+        c = fgetc(fpt);
+        if(c == ' ' || c == '\n' || c == EOF)
+        {
+            c = fgetc(fpt);
+            if(c == EOF)
+                break;
+        }
+        m->cells[i] = (c - '0');
+    }
+    fclose(fpt);
+}
+
+void Map_dtor(Map *m)
+{
+  free(m->cells);
+}
+
+
+bool Test_map(Map *m, int r, int c)
+{
+    bool test;
+    unsigned char border = 0;
+    while(border < 7)
+    {
+        dec_to_bin(&border);
+
+
+        border++;
+    }
+    return test;
+}
+
+bool is_border(Map *m, int r, int c, int border)
+{
+    bool test = true;
+
+    
+
+    return test;
+}
+
+void dec_to_bin(unsigned char *number)
+{
+    unsigned char temp_number;
+    temp_number = *number;
+
+    while()
+    {
+        if((*number % 2) == 1)
+        {
+            
+        }
+    }
+}
 
 void help()
 {
